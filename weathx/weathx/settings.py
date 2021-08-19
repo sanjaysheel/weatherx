d8df2a42498258df7c270bb559ab0ee3ef8400d8
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2021-08-19 17:56:36 +0530

    Update settings.py

--- weathx/weathx/settings.py
+++ weathx/weathx/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=e!dc3ahnkn-r2b@qxdozw+u$hyjmgabv%xrgdp^_=an_cy_v#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
